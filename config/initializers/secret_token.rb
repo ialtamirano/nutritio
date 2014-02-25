35bdd5025aade23da5c35cd9b41378c8b6a28d86
ivan altamirano <ivan.altamirano@gmail.com>
2014-02-25 17:18:02 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nutritio::Application.config.secret_key_base = '8c185f87fc9637222690490c932a6f1c3dedf716c6209d833db905db8f131c44e3e84981c4b4f467890d62edb0fe6236515210f470cfd9c5b97572460be3db91'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nutritio::Application.config.session_store :cookie_store, key: '_nutritio_session'
